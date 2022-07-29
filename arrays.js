a82fe26baccee46e6ccd97a64fda3d06dc664e0b
braian.anze <braiananze3@gmail.com>
2022-07-28 23:35:26 -0300

    'ejemplos'

--- arrays.js
+++ arrays.js
@@ -15,3 +15,62 @@ console.log({ primero,ultimo
 juegos.forEach((elemento,indice,arr)=> {
     console.log({elemento,indice,arr});
 });
+
+
+
+
+// ejemplos nuevos 
+
+
+const frutas = [ 'Manzana', 'Pera', 'Piña'];
+
+const otrasFrutas = frutas;
+
+otrasFrutas.push('Mango');
+
+console.table( {frutas, otrasFrutas} );
+
+
+
+
+const frutas2 = ['sandía','durazno','piña'];
+
+const otrasFrutas2 = [...frutas2];
+
+otrasFrutas2.push('cereza');
+
+console.table({frutas2,otrasFrutas2});
+
+
+
+
+
+
+
+let dia = 0
+
+if( dia === 0){
+    console.log('Domingo')
+} else {
+    console.log('no es el dia correcto')
+}
+
+
+
+
+const hoy = new Date();
+
+let nuevoDia = hoy.getDay(); 
+
+
+const diasLetras = {
+0: 'domingo', 
+1: 'lunes', 
+2: 'martes', 
+3: 'miercoles',
+4: 'jueves', 
+5: 'viernes', 
+6: 'sábado',
+} 
+
+console.log(diasLetras[nuevoDia] || 'Dia no definido');
