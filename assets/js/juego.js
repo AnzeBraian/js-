70b47878d4ac94b35e12b6180d62682a716a88a2
braian.anze <braiananze3@gmail.com>
2022-08-17 00:03:14 -0300

    actualización

---
+++ assets/js/juego.js
@@ -0,0 +1,73 @@
+
+
+let deck         = [];
+const tipos      = ['C','D','H','S']
+const especiales = ['A','J','Q','K']
+
+let puntosJugador = 0,
+    puntosComputadora = 0;
+
+
+
+const btnPedir = document.querySelector( '#btnPedir');
+
+const puntosHtml = document.querySelectorAll('small');
+
+
+const crearDeck = () => {
+
+    for( let i = 2; i <= 10; i++){
+        for(let tipo of tipos ){
+            deck.push(i + tipo);
+        }
+    }
+
+    for(let tipo of tipos) {
+        for(let esp of especiales){
+            deck.push(esp + tipo)
+        }
+    }
+
+    deck = _. shuffle ( deck)
+    console.log(deck)
+    return deck;
+}
+
+
+crearDeck();
+
+const pedirCarta = () =>{
+    if (deck.length === 0 ) {
+        throw 'No hay cartas en el deck'
+    }
+
+    const carta = deck.pop();
+    return carta;
+}
+
+
+
+
+const valorCarta = ( carta ) => {
+
+    const valor = carta.substring(0, carta.length - 1);
+    return ( isNaN (valor)) ? 
+            (valor === 'A') ? 11 : 10
+            : valor * 1;   
+
+
+
+
+
+}
+
+
+
+btnPedir.addEventListener('click', () => {
+    const carta = pedirCarta();
+
+    puntosJugador = puntosJugador + valorCarta ( carta);
+    puntosHtml[0].innerText = puntosJugador;
+
+
+})
